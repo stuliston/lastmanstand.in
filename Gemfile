b380d1e4933cb4701dbab8c21dc2b84170b0e59b
Stuart Liston <stuart.liston@gmail.com>
2013-04-21 20:27:24 +1000

    Added new rails4 project and a few gems

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+ruby '2.0.0'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0.beta1'
+gem 'thin'
+gem 'pg'
+gem 'cancan'
+gem 'jquery-rails'
+gem 'ember-rails'
+gem 'active_model_serializers'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 4.0.0.beta1'
+  gem 'coffee-rails', '~> 4.0.0.beta1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'awesome_print'
+  gem 'pry'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LastmanstandIn::Application.config.secret_key_base = '3e5e4ad3aaa624f6f8527f26e15e4ef240a4b6a0b77851c361730c5d84f990d26673c989c9d76edb178809eb5459e1c03f3b5f2f1a14fd11767c3cebe2a68797'
