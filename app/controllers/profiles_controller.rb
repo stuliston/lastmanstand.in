ab4848fef3139cf90b770d6502434f8601162917
Stuart Liston <stuart.liston@gmail.com>
2013-04-28 17:02:17 +1000

    Profiles index endpoint is sorted gov'nor

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,9 @@
 class ProfilesController < ApplicationController
   respond_to :json
-  #load_and_authorize_resource
+  load_and_authorize_resource
 
   def index
+    respond_with @profiles
   end
 
   def show
