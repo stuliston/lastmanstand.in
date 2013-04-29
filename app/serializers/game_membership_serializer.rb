511329568713ac047798738e29013ca727a32fff
Stuart Liston <stuart.liston@gmail.com>
2013-04-29 22:11:17 +1000

    Removed silly JSON

--- app/serializers/game_membership_serializer.rb
+++ app/serializers/game_membership_serializer.rb
@@ -2,12 +2,9 @@ class GameMembershipSerializer < ActiveModel::Serializer
 
   embed :ids, include: true
 
-  attributes :id, :avatar_image_url
+  attributes :id
 
   has_one :game
   has_one :profile
 
-  def avatar_image_url
-    'poop'
-  end
 end
