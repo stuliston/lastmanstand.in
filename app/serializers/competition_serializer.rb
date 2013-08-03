96bc7f7a56d2fa1e34a95d9b6cf52ea9504d53f2
Rob Monie <robmonie@gmail.com>
2013-08-03 19:10:14 +1000

    Loading competitions via the rest api loads less stuff.

--- app/serializers/competition_serializer.rb
+++ app/serializers/competition_serializer.rb
@@ -2,7 +2,7 @@ class CompetitionSerializer < ActiveModel::Serializer
 
   attributes :id, :name
 
-  has_many :seasons, embed: :ids, include: true
+  has_many :seasons, embed: :ids, include: true, serializer: ShallowSeasonSerializer
   has_one :current_season, embed: :ids
 
 end
---
+++ app/serializers/shallow_season_serializer.rb
@@ -0,0 +1,9 @@
+class ShallowSeasonSerializer < ActiveModel::Serializer
+
+  embed :ids, include: true
+  attributes :id, :name
+
+  has_one :competition
+  # has_many :rounds
+
+end
