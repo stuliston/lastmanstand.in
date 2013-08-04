46649e311c9f7a1e201c36e4c60de3b2182efde6
Stuart Liston <stuart.liston@gmail.com>
2013-08-04 17:55:22 +1000

    Fixing up seeds so that profile is created after user. Also, altering comp names to match Playup. This can be changed if we don't like thos names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
 
 User.destroy_all
 Profile.destroy_all
-stu = User.create!(email: 'stuart.liston@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.create!(name: 'Stu'))
-rob = User.create!(email: 'robmonie@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.create!(name: 'Rob'))
-phil = User.create!(email: 'metcalfe.phil@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.create!(name: 'Phil'))
-ash = User.create!(email: 'ash@ashmckenzie.org', password: 'password', password_confirmation: 'password', profile: Profile.create!(name: 'Ash'))
-dan = User.create!(email: 'locusdelicti@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.create!(name: 'Dan'))
+stu = User.create!(email: 'stuart.liston@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.new(name: 'Stu'))
+rob = User.create!(email: 'robmonie@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.new(name: 'Rob'))
+phil = User.create!(email: 'metcalfe.phil@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.new(name: 'Phil'))
+ash = User.create!(email: 'ash@ashmckenzie.org', password: 'password', password_confirmation: 'password', profile: Profile.new(name: 'Ash'))
+dan = User.create!(email: 'locusdelicti@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.new(name: 'Dan'))
 
 all_users = [ stu, rob, ash, phil, dan ]
 
+# Changed by Stu to match names from Playup. We can work around this if necessary
 Competition.destroy_all
-afl = Competition.create!(name: 'Australian Football League')
-epl = Competition.create!(name: 'English Premier League')
+afl = Competition.create!(name: 'AFL')
+epl = Competition.create!(name: 'Premier League')
 
 Team.destroy_all
 adelaide          = Team.create!(name: 'Adelaide', competition: afl)
