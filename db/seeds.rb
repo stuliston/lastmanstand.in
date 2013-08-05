edd8666284d8d0ad35d105431dbd1a5a7471aa31
Stuart Liston <stuart.liston@gmail.com>
2013-08-05 20:31:17 +1000

    Pulling comps from config.yml for consistency. This represents the mapping between our comps and theirs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@ User.create!(email: 'metcalfe.phil@gmail.com', password: 'password', password_co
 User.create!(email: 'ash@ashmckenzie.org', password: 'password', password_confirmation: 'password', profile: Profile.new(name: 'Ash'))
 User.create!(email: 'locusdelicti@gmail.com', password: 'password', password_confirmation: 'password', profile: Profile.new(name: 'Dan'))
 
-# Changed by Stu to match names from Playup. We can work around this if necessary
+
 Competition.destroy_all
-afl = Competition.create!(name: 'AFL')
-epl = Competition.create!(name: 'Premier League')
+
+LastManStanding.config.playup_api.competitions.each do |comp|
+  Competition.create!(name: comp.name)
+end
