fb72205e8bdb83cf619461ab2551d47534e7b989
Stuart Liston <stuart.liston@gmail.com>
2013-08-29 20:14:46 +1000

    Updating lost_lives with migration so we don't have to re-seed the DB

--- db/migrate/20130827085656_add_lost_lives_to_game_memberships.rb
+++ db/migrate/20130827085656_add_lost_lives_to_game_memberships.rb
@@ -1,5 +1,6 @@
 class AddLostLivesToGameMemberships < ActiveRecord::Migration
   def change
     add_column :game_memberships, :lost_lives, :integer
+    GameMembership.all.each(&:update_lives!)
   end
 end
