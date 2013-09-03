b7615da2be66f5b535e4015f155f5b1208ea0200
Rob Monie <robmonie@gmail.com>
2013-09-03 19:16:39 +1000

    see if we can get assets precompile on deploy

---
+++ deploy/before_symlink.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+ENV['RAILS_ENV'] = node[:deploy][:lms][:rails_env]
+current_release = release_path
+
+execute "rake assets:precompile" do
+  cwd current_release
+  command "bundle exec rake assets:precompile"
+  environment "RAILS_ENV" => node[:deploy][:lms][:rails_env]
+end
