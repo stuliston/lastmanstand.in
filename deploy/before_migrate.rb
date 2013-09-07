72c1e0ce15b90e7860642aa1edd28e0a35477be3
Rob Monie <robmonie@gmail.com>
2013-09-07 16:56:57 +1000

    adjust asset precompile deploy hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,9 +3,9 @@ Chef::Log.info("Running deploy/before_symlink.rb...")
 
 Chef::Log.info("Symlinking #{release_path}/public/assets to #{new_resource.deploy_to}/shared/assets")
 
-link "#{release_path}/public/assets" do
-  to "#{new_resource.deploy_to}/shared/assets"
-end
+# link "#{release_path}/public/assets" do
+#   to "#{new_resource.deploy_to}/shared/assets"
+# end
 
 rails_env = new_resource.environment["RAILS_ENV"]
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
