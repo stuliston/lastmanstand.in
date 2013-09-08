cd9e33ee38489e212fd45ffdcceca88078498d2d
Stuart Liston <stuart.liston@gmail.com>
2013-09-08 16:26:09 +1000

    Removing the whenever gem, we'll do cron with chef

--- deploy/after_restart.rb
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-Chef::Log.info("Running deploy/after_restart.rb...")
-
-rails_env = new_resource.environment["RAILS_ENV"]
-Chef::Log.info("Applying crontab")
-
-execute "whenever -w" do
-  command "whenever -w"
-end
